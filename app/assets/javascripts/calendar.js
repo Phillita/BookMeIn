6cef81eb6dec8550f870e06b8775dfbb216b0786
Tayler Phillips <tphillips@infotech.com>
2015-12-08 12:22:50 -0500

    Commented out static events. Fixed the time formatting on the calendar params.

--- app/assets/javascripts/calendar.js
+++ app/assets/javascripts/calendar.js
@@ -1,7 +1,7 @@
 setupCalendar = function() {
   $('#calendar').fullCalendar({
 			header: {
-				left: 'prev,next today',
+				left: 'next today',
 				center: 'title',
 				right: 'month,agendaWeek,agendaDay'
 			},
@@ -23,62 +23,37 @@ setupCalendar = function() {
           dow: $('#calendar').data('dow')
       },
 			events: [
-				{
-					title: 'All Day Event',
-					start: moment().format('YYYY-MM-DD'),
-          source: {}, // This is where the object from the db can go
-          description: 'CUSTOM' // Not standard, will not be displayed by default
-				},
-				{
-					title: 'Long Event',
-					start: '2015-12-07',
-					end: '2015-12-10'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-12-09T16:00:00'
-				},
-				{
-					id: 999,
-					title: 'Repeating Event',
-					start: '2015-12-16T16:00:00'
-				},
-				{
-					title: 'Conference',
-					start: '2015-12-11',
-					end: '2015-12-13'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-12-12T10:30:00',
-					end: '2015-12-12T12:30:00'
-				},
-				{
-					title: 'Lunch',
-					start: '2015-12-12T12:00:00'
-				},
-				{
-					title: 'Meeting',
-					start: '2015-12-12T14:30:00'
-				},
-				{
-					title: 'Happy Hour',
-					start: '2015-12-12T17:30:00'
-				},
-				{
-					title: 'Dinner',
-					start: '2015-12-12T20:00:00'
-				},
-				{
-					title: 'Birthday Party',
-					start: '2015-12-13T07:00:00'
-				},
-				{
-					title: 'Click for Google',
-					url: 'http://google.com/',
-					start: '2015-12-28'
-				}
+				// {
+				// 	title: 'All Day Event',
+				// 	start: moment().format('YYYY-MM-DD'),
+        //   source: {}, // This is where the object from the db can go
+        //   description: 'CUSTOM' // Not standard, will not be displayed by default
+				// },
+				// {
+				// 	title: 'Long Event',
+				// 	start: '2015-12-07',
+				// 	end: '2015-12-10'
+				// },
+				// {
+				// 	id: 999,
+				// 	title: 'Repeating Event',
+				// 	start: '2015-12-09T16:00:00'
+				// },
+				// {
+				// 	id: 999,
+				// 	title: 'Repeating Event',
+				// 	start: '2015-12-16T16:00:00'
+				// },
+				// {
+				// 	title: 'Meeting',
+				// 	start: '2015-12-12T10:30:00',
+				// 	end: '2015-12-12T12:30:00'
+				// },
+				// {
+				// 	title: 'Click for Google',
+				// 	url: 'http://google.com/',
+				// 	start: '2015-12-28'
+				// }
 			],
       eventRender: function(event, element) {
         if(event.description) {
